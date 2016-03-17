872ca9618b04441cff213262b0058c87f61217df
Vivek Pandya <vivekpandya@viveks-macbook-pro.local>
2016-03-16 19:48:54 -0700

    Initial Commit

---
+++ Q/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Q
+//
+//  Created by Vivek Pandya on 3/16/16.
+//  Copyright © 2016 Vivek Pandya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
